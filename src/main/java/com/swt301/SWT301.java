7d86ac314bce1e4192fc102e0ed164d5ffa5071b
LE ANH TUAN 180303 <leanhtuan.18.03.03@gmail.com>
2026-01-19 21:34:08 +0700

    init code master

---
+++ src/main/java/com/swt301/SWT301.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.swt301;
+
+/**
+ *
+ * @author LE ANH TUAN
+ */
+public class SWT301 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
